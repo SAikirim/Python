026652eb1fdd5a5a2e3e8ce18e316fb266a6d7ad
SAikirim <kyj9724@gmial.com>
2020-08-04 10:59:18 +0000

    python

--- test2/test.py
+++ test2/test.py
@@ -1,30 +1,28 @@
-nums = input("진짜 구술 수, 가짜 구술 수, 최대 사람 수 : ")
-nums = nums.split(' ')
+#
+# 마법의 구슬
+# https://www.acmicpc.net/problem/1095
+#
 
-if ((0 < int(nums[0] or nums[1]) <= 1000000000) and (0 < int(nums[2]) <= 1000000)) :
-    print("ok")
-else:
+nums = input("진짜 구술 수, 가짜 구술 수, 최대 사람 수 : ").split()
+
+if not ((0 < int(nums[0] or nums[1]) <= 1000000000) and (0 < int(nums[2]) <= 1000000)) :
     print("USAGE: [NUM] [NUM] [NUM](0 < NUM < 1000,000")
 
+S, F, M = map(int,nums)
 
-S=int(nums[0])
-F=int(nums[1])
-M=int(nums[2])
-
+# 최대 경우의 수 구하기
 Max=0
 for f in range(1, F+1):
     Max = S * f + Max
 
+# 최대 사람 수 구하기
 Max+=1
 try:
-    print("Max: " , Max)
-    for m in range(M, 1, -1):
-        print("m: " , m)
+    for m in range(M, 0, -1):
         p = Max%m
-        print("p: " , p)
         if  0 == p:
             break
-        else 1 == m:
+        elif 1== m :
             print("1")
     print(m)
 except:
