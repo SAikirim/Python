b48d92de7137ff06444461168b556d3ab3c8d7f2
SAiki <kyj9724@gmial.com>
2020-08-13 07:57:33 +0000

    miniter

--- api/app.py
+++ api/app.py
@@ -79,3 +79,15 @@ def unfollow():
     return jsonify(user)
 
 
+@app.route("/timeline/<int:user_id>", methods = ['GET'])
+def timeline(user_id):
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+    follow_list = app.users[user_id].get('follow', set())
+    follow_list.add(user_id)
+    timeline = [tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
+
+    return jsonify({
+        'user_id'   : user_id,
+        'timeline'  : timeline
+    })
