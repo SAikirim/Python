25cdc0e5b7a3a2d9e8ee698a4d6de13d507f6356
SAiki <kyj9724@gmail.com>
2020-09-07 13:59:35 +0900

    update 13:59

--- Coding_test/greedy_큰수의법칙.py
+++ Coding_test/greedy_큰수의법칙.py
@@ -1,8 +1,17 @@
+'''
+n = 배열 크기(수)
+m = 더하는 개수
+k = 연속해서 더할 수 있는 개수
+가장 큰수들을 더해 최대값 구하기
+가장 큰수는 연속해서 더하는데에 제한이 있음
+'''
+
 n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
 
-data.sort(reverse=True) 
+data.sort(reverse=True)
 
+'''
 resurt = 0
 while m != 0 :
   for j in range(k):
@@ -12,3 +21,11 @@ while m != 0 :
   m -= 1
 
 print(resurt)
+'''
+count = m//(k+1) * k
+count += m %(k+1)
+
+result = count * data[0]
+result += (m - count) * data[1]
+
+print(result)
