44db9682893cf9ab608242254c7bf3c9684c56fc
SAiki <kyj9724@gmail.com>
2020-09-18 09:18:35 +0900

    update 09:18

--- Coding_test/CreateGame.py
+++ Coding_test/CreateGame.py
@@ -9,24 +9,57 @@
 #
 
 # input data
-x, y = map(int, input("map_size: ").split())
-char = map(int, input("(x,y) dt : ").split())
-maps = [list(map(int, input().split())) for i in range(y) ]
+#x, y = map(int, input("map_size: ").split())
+x, y = 4, 4
+#char = list(map(int, input("(x,y) dt : ").split()))
+char = [1, 1, 0]
+#maps = [list(map(int, input().split())) for i in range(y) ]
+maps = [[1,1,1,1],[1,0,0,1],[1,1,0,1],[1,1,1,1]]
+d = [[0] * y for _ in range(x)]
+d[char[0]][char[1]] = 1
 
 # set data
 # 바라보는 방향 - 북:0 동:1 남:2 서:3
-dt = [0, 1, 2, 3]
+dy = [0, 1, 0, -1]
+dx = [-1, 0, 1, 0]
 
 # 1
-def trun_left_mv():
-   char[2] = (char[2] + 1) % 4
-   if char[2] == 0 :
-       char[1] -= 1
-   if char[2] == 1 :
-       char[0] -= 1
-   if char[2] == 2 :
-       char[1] += 1
-   if char[2] == 3 :
-       char[0] += 1
+def turn_left_mv():
+    global char
+    char[2] -= 1
+    if char[2] == -1:
+        char[2] = 3
 
+for i in maps:
+    print(i)
 
+# start move
+count = 1
+turn_time = 0
+while True:
+    turn_left_mv()
+    nx = char[0] + dx[char[2]]
+    ny = char[1] + dy[char[2]]
+    # check land
+    if maps[nx][ny] == 0 and d[nx][ny] == 0:
+        char[0] = nx
+        char[1] = ny
+        d[nx][ny] = 1
+        count += 1
+        turn_time = 0
+        continue
+    # if sea and went land
+    else:
+        turn_time += 1
+    # Not Forward
+    if turn_time == 4:
+        nx = char[0] - dx[char[2]]
+        ny = char[2] - dy[char[2]]
+        if d[nx][ny] == 0:
+            x = nx
+            y = ny
+            turn_time = 0
+        else:
+            break
+
+print(count)
