8a57ae4a1de25e50fa0ee3bc010861468bf86b6a
JongCheol Yim <kyj9724@ssrinc.co.kr>
2022-04-21 14:14:08 +0900

    [add]vscode로 파이썬 디버깅 설정

--- test2/test.py
+++ test2/test.py
@@ -1,29 +1,8 @@
+#!/usr/bin/env python3
+
 #
 # 마법의 구슬
 # https://www.acmicpc.net/problem/1095
 #
 
-nums = input("진짜 구술 수, 가짜 구술 수, 최대 사람 수 : ").split()
-
-if not ((0 < int(nums[0] or nums[1]) <= 1000000000) and (0 < int(nums[2]) <= 1000000)) :
-    print("USAGE: [NUM] [NUM] [NUM](0 < NUM < 1000,000")
-
-S, F, M = map(int,nums)
-
-# 최대 경우의 수 구하기
-Max=0
-for f in range(1, F+1):
-    Max = S * f + Max
-
-# 최대 사람 수 구하기
-Max+=1
-try:
-    for m in range(M, 0, -1):
-        p = Max%m
-        if  0 == p:
-            break
-        elif 1== m :
-            print("1")
-    print(m)
-except:
-    print("-1")
+print("test")
